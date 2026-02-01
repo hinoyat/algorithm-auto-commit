645528aa35cac9725b045222c69ec50b5666e01b
Hyeon Ho <hhwj2270@gmail.com>
2026-02-02 00:12:34 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 택배 상자 꺼내기 - 389478 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389478?language=java) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.05 ms
+메모리: 80.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 17일 15:45:03
+2026년 02월 02일 00:12:29
 
 ### 문제 설명
 
--- 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -1,45 +1,41 @@
+import java.util.*;
+
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 1;
-        
-        int h = (n + w - 1) / w;
-        
-        int[][] arr = new int[h][w];
-        
-        int number = 1;
-        
-        int startX = 0;
-        int startY = 0;
-        
-        int way = 0;
-        
-        for (int i = h - 1; i >= 0 && number <= n; i--) {
-            int j   = (way == 0) ? 0 : w - 1;
-            int dir = (way == 0) ? 1 : -1;
+        int height = (n / w) + 1;
+        int[][] arr = new int[height][w];
 
-            for (int step = 0; step < w && number <= n; step++, j += dir) {
-                arr[i][j] = number;
-                if (number == num) {
-                    startX = i;
-                    startY = j;
+        int count = 1;
+        int col = 0;
+        int row = 0;
+
+        for (int i = 0; i < height; i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < w && count <= n; j++) {
+                    if (count == num) {
+                        col = i;
+                        row = j;
+                    }
+                    arr[i][j] = count++;
                 }
-                number++;
+            } else {
+                for (int j = w - 1; j >= 0 && count <= n; j--) {
+                    if (count == num) {
+                        col = i;
+                        row = j;
+                    }
+                    arr[i][j] = count++;
+                }
             }
-            way = 1 - way;
         }
-        
-        while (true) {
-            startX --;
-            
-            if (startX < 0) {break;}
-            
-            if (arr[startX][startY] != 0) {
-                answer ++;    
+
+        int answer = 0;
+        for (int i = col; i < height; i++) {
+            if (arr[i][row] != 0) {
+                answer++;
             }
-            
         }
-        
+
         return answer;
     }
-
 }
